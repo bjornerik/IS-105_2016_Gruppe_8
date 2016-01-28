20a6056116d3dd42b0617431d5d3630ae9c8fb8f
Lani05 <laniblablabla@yahoo.no>
2016-01-28 15:25:57 +0100

    Oppgave uke 4.py
    Funker

---
+++ Oppgaver uke 4.py
@@ -0,0 +1,9 @@
+Oppgave 1.2.2.
+a: Det tar mindre plass å sende og laste opp,
+   og sparer maskiner for ressurser og folk for tid.
+   
+b: 00101001100000 blir
+   xxyyxzxxxxx
+   
+c: Nå var du optimistisk Janis, smiil.   
+   
