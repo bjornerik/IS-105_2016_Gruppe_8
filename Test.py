4d99fa5d5b7646a208a44d82db97b84d9124e9a9
hovisjur <hovisjur@gmail.com>
2016-02-04 11:11:38 +0100

    Endring

--- Test.py
+++ Test.py
@@ -1,2 +1,9 @@
-print "testyoloswag"
-print "hei"
+str = raw_input("Enter code")
+#str = "0" + str[2:]
+message = ""
+while str != "":
+    i = chr(int(str[:8],2))
+    message=message+i
+    str=str[8:]
+    
+print message
