4f9619e4e6d153e09fb1d3a64b944fd67303a43a
hovisjur <hovisjur@gmail.com>
2016-02-07 17:27:33 +0100

    Gruppe8

---
+++ Gruppe8.py
@@ -0,0 +1,16 @@
+def main ():
+    x = int(input("Velg enten 0, 10 eller 11: "))
+    
+    if (x == 0):
+        print ('A')
+        
+    elif (x == 10):
+        print ('B')
+        
+    elif (x == 11):
+        print ('C')
+        
+    else:
+        print ('Ditt valg er ikke gyldig')
+  
+main()
