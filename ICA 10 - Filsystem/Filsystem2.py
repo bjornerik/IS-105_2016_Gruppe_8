c5026eeede17971d05366bce40d8c4598c962df0
Hovisjur <hovisjur@gmail.com>
2016-03-30 11:09:00 +0200

    ICA 10: Nesten ferdig
    Har implementert en fullstendig loop med de fleste funksjoner. Det er
    fremdeles noen bugs på redigering av fil, må også vurdere å ta inn igjen
    mmap.
    Den viktigste endringen fra planen er at jeg har lagt inn alt av fil
    funksjoner på mappenivå, slik at filen åpnes for hver enkelte operasjon,
    og at man etter valgt operasjon, må velge hvilken fil man vil utføre den
    i.

--- ICA 10 - Filsystem/Filsystem2.py
+++ ICA 10 - Filsystem/Filsystem2.py
@@ -1,63 +1,151 @@
 import os.path
+import sys
 import mmap
 
+os.chdir("C:\\Users\Sjur\Documents\Filsystem")   
+
+def isdir(dirname):
+    return os.path.exists(dirname)
+
+def isfile(fil):
+    return os.path.isfile(fil)
+
 class Folder(object):
-    pass
-
+    def __init__(self, name):
+        self.name = name
+        if not os.path.exists(self.name):
+            os.mkdir(self.name) 
+        
 class File(object):
-    pass
-
-# write a simple example file
-with open("hello.txt", "wb") as f:
-    f.write(b"Hello Python!\n")
-
-with open("hello.txt", "r+b") as f:
-    # memory-map the file, size 0 means whole file
-    mm = mmap.mmap(f.fileno(), 0)
-    # read content via standard file methods
-    print(mm.readline())  # prints b"Hello Python!\n"
-    # read content via slice notation
-    print(mm[:5])  # prints b"Hello"
-    # update content using slice notation;
-    # note that new content must have same size
-    mm[6:] = b" world!\n"
-    # ... and read again using standard file methods
-    mm.seek(0)
-    print(mm.readline())  # prints b"Hello  world!\n"
-    # close the map
-    mm.close()
-
-def isFile(fil):
-    if os.path.isfile(fil):
-        print (fil + " finnes")
-    else:
-        print(fil + "Finnes ikke")
+    def __init__(self, navn):
+        self.navn = navn
+        file = open((navn + ".txt"), "w")
+        file.write(" ")
+        file.close()  
+        
+def write(name, text):
+    with open(name) as f:
+        f.write(text)
+        f.close()
+        
+def read(name):
+    with open(name) as f:
+        print(f.read())
+        f.close()
+        
+def searchWord(name, word):
+    with open(name) as f:
+        found = False
+        while not found:
+            for n in f:
+                if n == word:
+                    print (word + " is found at line" + len(n))
+                    found = True
+            print(word + " was not found in the file")
+        f.close()
 
     
 def displayCommands(path):
-    if path == "3":
-        print("The following commands is possible")
+    print("The following commands is possible")
+    if path == "1":
+        print("isdir")
+        print("opendir")
+        print("createdir")
+        print("quit")
+    if path == "2":
         print("isfile")
+        print("cd..")
+        print("createfile")
+        print("write")
+        print("read")
+        print("searchWord")
+        print("quit")
     
 def fileloop():
-    path = ["C", "M", "fil"]
-    print (path)
+    x = True
+    count = 1
     
-    while True:
+    
+    while x:
+        path = os.getcwd()
         print ("You are here: ")
-        for element in path:
-            print (element + "/")
+        #print (" \ ".join(path))
+        print (path)
         print ("Type cmd to display possible commands")
         command = input(">")
-        if len(path) == 2:
+        if command == "quit":
+            x = False
+        if count == 1:
             if command == "cmd":
-                displayCommands("3")
+                displayCommands("1")
+            if command == "isdir":
+                thedirectory = input("Which folder do you want to check if exist?\n")
+                if isdir(thedirectory):
+                    print(thedirectory + " finnes")
+                else:
+                    print(thedirectory + " finnes ikke")                    
+            if command == "opendir":
+                thedirectory = input("Which folder do you want to open?\n")
+                if isdir(thedirectory):
+                    count = 2
+                    os.chdir(thedirectory)
+                else: 
+                    print ("The folder you tried to open doesn't exist")
+
+            if command == "createdir":
+                navn = input("What do you want to name the folder?\n")
+                Folder(navn)
+        
+        if count == 2:
+            if command == "cmd":
+                displayCommands("2")            
             if command == "isfile":
-                file = input("Which file do you want to check if exists?")
-                isFile(file)
+                thefile = input("Which file do you want to check if exist?\n")
+                if isfile(thefile):
+                    print(thefile + " finnes")
+                else:
+                    print(thefile + " finnes ikke")
+                    
+            if command == "cd..":
+                count = 1
+                os.chdir(path)
+                    
+            if command == "createfile":
+                filnavn = input("What do you want to name the file?\n")
+                File(filnavn)
+            
+            if command == "write":
+                thefile = input("Which file do you want to write in?\n")
+                if isfile(thefile):
+                    text = input("What do you want to write?\n")
+                    write(thefile, text)
+                    print (text + "\nwas written in " + thefile)
+                else:
+                    print ("The file you tried to open doesn't exist")
+            
+            if command == "read":
+                thefile = input("Which file do you want to read?\n")
+                if isfile(thefile):
+                    read(thefile)
+                else:
+                    print ("The file you tried to open doesn't exist")        
+            
+            if command == "searchWord":
+                thefile = input("Which file do you want to search in?\n")
+                if isfile(thefile):
+                    word = input("What word do you want to search for?\n")
+                    searchWord(thefile, word)
+                else:
+                    print ("The file you tried to open doesn't exist") 
+                
+fileloop()
+                
+        
+        
+        
 
 
             
-fileloop()
+
             
         
