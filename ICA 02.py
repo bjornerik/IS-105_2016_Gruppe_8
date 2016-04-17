33a4e62c7f11f0b44ec75c6b65598d6706529799
Hovisjur <hovisjur@gmail.com>
2016-04-17 22:38:25 +0200

    Update ICA 02
    La til input og omgjøring av input til liste slik at man kan oversette fortløpende.

--- ICA 02.py
+++ ICA 02.py
@@ -1,14 +1,16 @@
 def tolk():
-    liste = ['X','Y','Z', 'Y', 'Z']
-    string = []
-    for x in liste:
-        if x == "X":
-            string.append("01")
-        elif x == "Y":
-            string.append("10")
-        elif x == "Z":
-            string.append("11")
-    print string
+    while 1:
+        encoding = raw_input('Encode >')
+        liste = encoding.split()
+        string = []
+        for x in liste:
+            if x == "X":
+                string.append("01")
+            elif x == "Y":
+                string.append("10")
+            elif x == "Z":
+                string.append("11")
+        print string
     
 tolk()
     
